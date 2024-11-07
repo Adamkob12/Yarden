2843ec74b73238f3a92b38f19d14e7e73b7504ab
Adam <46227443+Adamkob12@users.noreply.github.com>
2024-11-07 11:23:35 +0200

    experimenting with audio

--- client/src/video_local.rs
+++ client/src/video_local.rs
@@ -2,6 +2,7 @@ use crate::frame_streamer::{Frame, FrameStreamer, FrameStreamerMetaData};
 use ffmpeg::{
     self, codec, decoder,
     format::{context::Input, input, Pixel},
+    frame::Audio,
     software::scaling::{flag::Flags, Context},
     util::frame::Video,
     Error,
@@ -14,8 +15,10 @@ pub struct LocalVideo {
     current_frame: usize,
     pub metadata: FrameStreamerMetaData,
     ictx: Input,
-    decoder: decoder::Video,
+    video_decoder: decoder::Video,
+    audio_decoder: decoder::Audio,
     video_stream_index: usize,
+    audio_stream_index: usize,
     scaler: Context,
 }
 
@@ -27,22 +30,32 @@ impl LocalVideo {
             file_name
         );
         let ictx = input(file_name).unwrap();
-        let input = ictx
+        let video_input = ictx
             .streams()
             .best(ffmpeg::media::Type::Video)
             .ok_or(Error::StreamNotFound)
             .unwrap();
-        let video_stream_index = input.index();
-        let context_decoder = codec::context::Context::from_parameters(input.parameters()).unwrap();
-        let decoder = context_decoder.decoder().video().unwrap();
+        let audio_input = ictx
+            .streams()
+            .best(ffmpeg::media::Type::Audio)
+            .ok_or(Error::StreamNotFound)
+            .unwrap();
+        let video_stream_index = video_input.index();
+        let audio_stream_index = audio_input.index();
+        let video_context_decoder =
+            codec::context::Context::from_parameters(video_input.parameters()).unwrap();
+        let audio_context_decoder =
+            codec::context::Context::from_parameters(video_input.parameters()).unwrap();
+        let video_decoder = video_context_decoder.decoder().video().unwrap();
+        let audio_decoder = audio_context_decoder.decoder().audio().unwrap();
 
         let scaler = Context::get(
-            decoder.format(),
-            decoder.width(),
-            decoder.height(),
+            video_decoder.format(),
+            video_decoder.width(),
+            video_decoder.height(),
             Pixel::BGRZ,
-            decoder.width(),
-            decoder.height(),
+            video_decoder.width(),
+            video_decoder.height(),
             Flags::BILINEAR,
         )
         .unwrap();
@@ -59,20 +72,22 @@ impl LocalVideo {
                 frame_height: yarden::get_height(file_name),
             },
             ictx,
-            decoder,
+            video_decoder,
+            audio_decoder,
             video_stream_index,
+            audio_stream_index,
         }
     }
 
     pub fn poll_next_frame(&mut self) -> Option<Video> {
         for (stream, packet) in self.ictx.packets() {
             if stream.index() == self.video_stream_index {
-                self.decoder.send_packet(&packet).unwrap();
+                self.video_decoder.send_packet(&packet).unwrap();
                 break;
             }
         }
         let mut decoded = Video::empty();
-        self.decoder.receive_frame(&mut decoded).ok()?;
+        self.video_decoder.receive_frame(&mut decoded).ok()?;
         debug_assert_eq!(decoded.width(), self.metadata.frame_width);
         debug_assert_eq!(decoded.height(), self.metadata.frame_height);
         let mut rgb_frame = Video::empty();
@@ -80,6 +95,24 @@ impl LocalVideo {
         self.current_frame += 1;
 
         Some(rgb_frame)
+    }
+
+    pub fn _poll_audio(&mut self) -> Option<Audio> {
+        for (stream, packet) in self.ictx.packets() {
+            if stream.index() == self.audio_stream_index {
+                self.audio_decoder.send_packet(&packet).unwrap();
+                break;
+            }
+        }
+        let mut decoded = Audio::empty();
+        self.audio_decoder.receive_frame(&mut decoded).ok()?;
+        println!(
+            "Recieved {} samples at {} sample rate.",
+            decoded.samples(),
+            decoded.rate()
+        );
+
+        Some(decoded)
     }
 }
 
